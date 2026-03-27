b6a5f46b4b21c96e692a32a98a27562fbc400f54
Daniel Ferreira <danikferreira69@gmail.com>
2026-03-26 21:38:13 -0300

    Bind form to Financeiro and add validations
    Add DataAnnotations and model binding: import System.ComponentModel.DataAnnotations and mark Descricao, Valor and DataDaOperacao as [Required] in Financeiro. Update AdicionarTransacao.cshtml to use @model Financeiro, add validation summary, bind inputs/selects with asp-for, set form to post to Home/AdicionarTransacao, and add submit/cancel buttons. Note: cancel link contains a stray 'hre"' typo that should be corrected to 'href'.

--- Models/Financeiro.cs
+++ Models/Financeiro.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace GerenciamentoFinanceiroCurso.Models
 {
     public class Financeiro
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Descrição não foi preenchido.")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Valor não foi preenchido.")]
         public double Valor { get; set; }
+        [Required(ErrorMessage = "Data de Operação não foi preenchido.")]
         public DateTime DataDaOperacao { get; set; }
         public string CategoriaId { get; set; }
         [ValidateNever]
